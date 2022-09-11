a8a4bdee8a9ca08fbcc2683bc0b37bd57e4d4288
Marco <marco.fox.marco@gmail.com>
2022-09-11 11:25:56 +0200

    button track

---
+++ js/analytics.js
@@ -0,0 +1,28 @@
+function track(detail) {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "click",
+        detail: detail,
+    });
+}
+$("#marlup_website").click(function (event) {
+    track("marlup website");
+});
+$("#btn_show_all_videos").click(function (event) {
+    track("show all videos");
+});
+$("#btn_show_all_photos").click(function (event) {
+    track("show all photos");
+});
+$("#btn_show_all_events").click(function (event) {
+    track("show all events");
+});
+$("#btn_show_all_events_header").click(function (event) {
+    track("show all events");
+});
+$("#volantino_img").click(function (event) {
+    track("show all events");
+});
+$("#nostri_corsi_btn").click(function (event) {
+    track("i nostri corsi");
+});
