3acbb29c916fcf78004dd3cab9ff1774aeff7249
Marco <marco.fox.marco@gmail.com>
2022-09-13 11:01:29 +0200

    fix

--- js/analytics.js
+++ js/analytics.js
@@ -5,10 +5,6 @@ function track(detail, event = "click_btn") {
         detail: detail,
     });
 }
-function openUrl(url) {
-    window.open(url, "_blank").focus();
-}
-
 $("#marlup_website").click(function (event) {
     track("marlup website");
 });
@@ -19,23 +15,15 @@ $("#btn_show_all_photos").click(function (event) {
     track("show all photos");
 });
 $("#btn_show_photos_2022").click(function (event) {
-    openUrl("https://www.facebook.com/media/set/?set=a.489594019747847&type=3");
     track("show photos 2022");
 });
 $("#btn_show_photos_2019a").click(function (event) {
-    openUrl(
-        "https://www.facebook.com/media/set/?set=a.2469574986486701&type=3"
-    );
     track("show photos 2019 a");
 });
 $("#btn_show_photos_2019b").click(function (event) {
-    openUrl(
-        "https://www.facebook.com/media/set/?set=a.1997567607020777&type=3"
-    );
     track("show photos 2019 b");
 });
 $("#btn_show_photos_2016").click(function (event) {
-    openUrl("https://www.facebook.com/media/set/?set=a.938714779572737&type=3");
     track("show photos 2016");
 });
 $("#btn_show_all_events").click(function (event) {
