3da5c561a8f63171c4e019db68184d4c25e1388f
Marco <marco.fox.marco@gmail.com>
2022-12-25 18:47:56 +0100

    fix

--- js/analytics.js
+++ js/analytics.js
@@ -8,18 +8,33 @@ function track(detail, event = "click_btn") {
 $(".marlup_website").click(function () {
     track("marlup website");
 });
-$("#wa-widget-send-button").click(function () {
-    track("open popup", "whatsapp");
-});
-$("#home_btn_info").click(function () {
-    track("open popup home", "whatsapp");
-});
-$(".wa-chat-box-content-send-btn").click(function () {
-    track("start chat", "whatsapp");
-});
 $("#write_review").click(function () {
     track("write review");
 });
 $("#home_btn_events").click(function () {
     track("home button events");
+});
+$("#home_btn_info").click(function () {
+    track("open popup home", "whatsapp");
+});
+$("#wa-widget-send-button").click(function () {
+    track("open popup", "whatsapp");
+});
+$(".wa-chat-box-content-send-btn").click(function () {
+    track(window.location.pathname, "whatsapp_lead");
+});
+$(".wa-chat-box-send").click(function () {
+    track(window.location.pathname, "whatsapp_lead");
+});
+$(".btn-prenota-ora").click(function () {
+    track(window.location.pathname, "whatsapp_lead");
+});
+$(".btn-prenota-ora-balli-di-gruppo").click(function () {
+    track("balli di gruppo", "whatsapp_lead");
+});
+$(".btn-prenota-ora-tango").click(function () {
+    track("tango", "whatsapp_lead");
+});
+$(".btn-prenota-ora-standard").click(function () {
+    track("standard, latino americano", "whatsapp_lead");
 });
